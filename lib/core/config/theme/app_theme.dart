fb25d00f7f4b2ed2763dbd6608c8ad7a53ae9b2e
Alejandro Loayza <alexloayza13@gmail.com>
2025-05-20 17:04:32 +0200

    dark theme

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -3,10 +3,29 @@ import 'package:spotifake/core/config/theme/app_colors.dart';
 
 class AppTheme {
 
-  static final ligthTheme = ThemeData(
+  static final lightTheme = ThemeData(
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.ligthBackground,
     brightness: Brightness.light,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        // foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+
+  static final darkTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
